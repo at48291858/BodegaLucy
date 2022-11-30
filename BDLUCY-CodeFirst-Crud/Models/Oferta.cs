2be9bfa8113ca1c37727b4e57829b21f34e1b5bc
Daniel Rodriguez <dan74109@gmail.com>
2022-11-30 18:10:18 -0500

    Actualice los modelos de Oferta y Cliente

--- BDLUCY-CodeFirst-Crud/Models/Oferta.cs
+++ BDLUCY-CodeFirst-Crud/Models/Oferta.cs
@@ -11,18 +11,20 @@ namespace BDLUCY_CodeFirst_Crud.Models
         //---------------------
         public int Codigo_Producto { get; set; }
         //---------------------
+        [Required(ErrorMessage ="El campo es obligatorio")]
         public int Cantidad_Oferta { get; set; } = 0;
         //---------------------
+        [Required(ErrorMessage = "El campo es obligatorio")]
         public float Descuento_Oferta { get; set; } = 0;
         //---------------------
-        [Column(TypeName = "nvarchar(100)")]
-        public string Descripcion_Oferta { get; set; } = "";
-        //---------------------
+        [Required(ErrorMessage = "El campo es obligatorio")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Fecha_Inicio_Oferta { get; set; }
         //---------------------
+        [Required(ErrorMessage = "El campo es obligatorio")]
         public DateTime Fecha_Final_Oferta { get; set; }
         //--------------------------------
         [ForeignKey("Codigo_Producto")]
-        public Producto producto { get; set; }
+        public Producto? producto { get; set; }
     }
 }
